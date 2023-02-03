24b86d1a146304148b6fe16d8273bb8f4641a5e4
Markus Schuss <markus.schuss@tugraz.at>
2023-02-03 10:25:02 +0000

    Updated Copyright to 2023, fixed indentation and comment style of html templates

--- dcube-web/src/app/models/jamming_scenario.py
+++ dcube-web/src/app/models/jamming_scenario.py
@@ -1,7 +1,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2022 Graz University of Technology
+# Copyright (c) 2023 Graz University of Technology
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
